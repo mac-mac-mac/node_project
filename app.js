1789666cb8bdd4bf5713b346c3e9aa5a81ba2a82
mac-mac-mac <macfbarron@gmail.com>
2022-10-17 18:15:08 +0000

    Created home.ejs and updated app.js

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var axios = require('axios');
 var app = express();
 
+app.use('/static', express.static("public"));
+app.set("view engine", "ejs");
+
+let randNum = Math.floor(Math.random() * 2682) + 1;
+const url = 'https://xkcd.com/' + randNum + '/info.0.json';
+
 app.get('/', function(req, res){
-    res.send('Hello World');
+    console.log(url);
+    axios.get(url).then((response) => {
+        console.log(response.data)
+        //res.render('home.ejs')
+    }).catch((error) => {
+        res.json({"Error: ": error})
+    })
 })
 
 app.listen(3000, function(){
-    console.log('App listening on port 3000')
+    console.log('App listening on port 3000');
 })
